628edfc3eee0fb280321d912b426279a08e32eda
Vel-Ant <vel-ant@yandex.ru>
2023-01-26 17:24:32 +0300

    Added reportComment & Tests

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -12,63 +12,67 @@ val sdfFormat = sdf.format(timestamp)
 val timestampApi = DateTimeFormatter.ISO_INSTANT.format(Instant.ofEpochSecond(timestamp / 1000))
 
 class PostNotFoundException(message: String): RuntimeException(message)
+class CommentNotFoundException(message: String): RuntimeException(message)
+class ReasonNotFoundException(message: String): RuntimeException(message)
 
 data class Post(
-    val id: Int,    // Идентификатор записи
+    var post_id: Int,    // Идентификатор записи
     val owner_id: Int,  // Идентификатор владельца стены, на которой размещена запись
     val from_id: Int,   // Идентификатор автора записи (от чьего имени опубликована запись)
     val text: String,   // Текст записи
-    val friends_only: Boolean?,  // true - если запись была создана с опцией «Только для друзей»
-    val can_pin: Boolean?,   // Информация о том, может ли текущий пользователь закрепить запись
-    val can_delete: Boolean?,    // Информация о том, может ли текущий пользователь удалить запись
-    val can_edit: Boolean?,  // Информация о том, может ли текущий пользователь редактировать запись
+    val friends_only: Boolean = false,  // true - если запись была создана с опцией «Только для друзей»
+    val can_pin: Boolean = true,   // Информация о том, может ли текущий пользователь закрепить запись
+    val can_delete: Boolean = true,    // Информация о том, может ли текущий пользователь удалить запись
+    val can_edit: Boolean = true,  // Информация о том, может ли текущий пользователь редактировать запись
     val attachments : Array<Attachment>? = emptyArray(),    // Вложение
-    val date: Long   // Время публикации записи в формате unixtime
+    val date: Long = timestamp  // Время публикации записи в формате unixtime
 )
 
 data class Comment(
-    val id: Int? = null,    // Идентификатор комментария
+    val comment_id: Int,    // Идентификатор комментария
     val from_id: Int? = null,   // Идентификатор автора комментария
     val text: String? = null,   // Текст комментария
-    val date: Long? = null   // Дата создания комментария в формате Unixtime
+    val date: Long = timestamp   // Дата создания комментария в формате Unixtime
 )
 data class User(
-    val id: Int,    // Идентификатор пользователя
+    val uid: Int,    // Идентификатор пользователя
     val first_name: String, // Имя
     val last_name: String   // Фамилия
 )
 
+data class Complaint(
+    val complaintComments : MutableList<Comment> = mutableListOf()
+)
+
 object UserAdd {
 
-    private var users = emptyArray<User>()
+    private var users = mutableListOf<User>()
     private var lastId = 0
 
-    fun add(user: User): User {
-        users += user.copy(id = ++lastId)
+    fun addUser(first_name: String, last_name: String): User {
+        users.add(User(++lastId, first_name = first_name, last_name = last_name))
         return users.last()
     }
 
-    fun printAll() {
-        for (user in users) {
-            println(user)
-        }
-    }
+    fun printAll() = users.forEach(:: println)
 }
 
 object WallService {
 
     private var posts = emptyArray<Post>()
     private var comments = emptyArray<Comment>()
-    private var lastId = 0
+    private var complaints = mutableMapOf<Int, Complaint>()
+    private var lastPostId = 0
+    private var lastCommentId = 0
 
     fun add(post: Post): Post {
-        posts += post.copy(id = ++lastId)
+        posts += post.copy(post_id = ++lastPostId)
         return posts.last()
     }
 
     fun update(newPost: Post): Boolean {
         for ((index, post) in posts.withIndex()) {
-            if (post.id == newPost.id) {
+            if (post.post_id == newPost.post_id) {
                 posts[index] = newPost.copy()
                 return true
             }
@@ -76,38 +80,58 @@ object WallService {
         return false
     }
 
-    fun createComment(postId: Int, comment: Comment): Comment {
+    fun createComment(post_id: Int, comment: Comment): Comment {
         for (post in posts) {
-            if (post.id == postId) {
-                println("Комментарий ${comment.id} добавлен к посту $postId")
-                return comment
+            if (post.post_id == post_id) {
+                comments += comment.copy(comment_id = ++lastCommentId)
+                return comments.last()
             }
         }
-        throw PostNotFoundException("no post with id $postId")
+        throw PostNotFoundException("Post with id $post_id not found")
     }
-    fun findById(id: Int): Post? {
-        for (post in posts) {
-            if (post.id == id) {
-                return post
-            }
-        }
-        return null
+
+    fun findById(post_id: Int): Post {
+      return posts.find { it.post_id == post_id} ?: throw PostNotFoundException("Post with id $post_id not found")
     }
 
     fun removeById(id: Int): Boolean {
         TODO("Unimplemented")
     }
 
-    fun printAll() {
-        for (post in posts) {
-            println(post)
+    fun reportComment(comment_id: Int, reason: Int): Boolean {
+
+        val reasonValue = when (reason) {
+            0 -> "спам"
+            1 -> "детская порнография"
+            2 -> "экстремизм"
+            3 -> "насилие"
+            4 -> "пропаганда наркотиков"
+            5 -> "материал для взрослых"
+            6 -> "оскорбление"
+            8 -> "призывы к суициду"
+            else -> throw ReasonNotFoundException("Reason with # $reason not found")
         }
+
+        for (comment in comments) {
+            if (comment.comment_id == comment_id) {
+                complaints.getOrPut(reason) {Complaint()}.complaintComments += comment
+                println("Комментарий помечен жалобой: \"$reasonValue\"")
+                return true
+            }
+        }
+        throw CommentNotFoundException("Comment with id $comment_id not found")
     }
 
+    fun printAllPosts() = posts.forEach(:: println)
+
+    fun printAllComments() = comments.forEach(:: println)
+
+    fun printAllComplaints() = complaints.forEach(:: println)
     fun clear() {
         posts = emptyArray()
         comments = emptyArray()
-        lastId = 0
+        lastPostId = 0
+        lastCommentId = 0
     }
 }
 
@@ -116,31 +140,23 @@ object reposts {    // Информация о репостах записи («
     var count = 0     // число пользователей, скопировавших запись;
     var userRepost = 0  // репост записи текущего пользователя;
 
-    fun calculation(user: User) {
+    fun calculation(uid: Int) {
         count++
-        userRepost = user.id
+        userRepost = uid
     }
 
-    fun user_reposted(user: User): Boolean {    // наличие репоста от текущего пользователя
-        return userRepost == user.id
-    }
+    fun user_reposted(uid: Int): Boolean { return userRepost == uid }   // наличие репоста от текущего пользователя
 
-    fun clear() {
-        count = 0
-    }
+    fun clear() { count = 0 }
 }
 
 object views {      // Информация о просмотрах записи
 
     var count = 0     // число просмотров записи
 
-    fun calculation(userId: User) {
-        count++
-    }
+    fun calculation(uid: Int) { count++ }
 
-    fun clear() {
-        count = 0
-    }
+    fun clear() { count = 0 }
 }
 
 fun main() {
@@ -153,26 +169,30 @@ fun main() {
     val attachmentAudio1 = AudioAttachment(audio1)
     println(attachmentAudio1.type)
 
-    val post = Post(1, 1, 2, "Bla-Bla", true, true, false, null, arrayOf(attachmentVideo1, attachmentAudio1), timestamp)
-    WallService.add(post)
-    WallService.add(post)
-    WallService.printAll()
-    println(WallService.update(Post(2, 1, 3, "Update Bla-Bla", true, true, false, false, arrayOf(attachmentVideo1, attachmentAudio1), timestamp)))
-    WallService.printAll()
+    val post = Post(0, 1, 2, "Bla-Bla", attachments = arrayOf(attachmentVideo1, attachmentAudio1))
+    println(WallService.add(post))
+    println(WallService.add(post))
+    WallService.update(Post(2, 1, 3, "Update Bla-Bla", attachments = arrayOf(attachmentVideo1, attachmentAudio1)))
+    WallService.printAllPosts()
 
-    val user = User(100, "Anton", "Velasco")
-    UserAdd.add(user)
-    UserAdd.printAll()
+    println(UserAdd.addUser("Anton", "Velasco"))
     println("Кол-во просмотров поста: ${views.count}")
-    views.calculation(user)
+    views.calculation(1)
     println("Кол-во просмотров поста: ${views.count}")
 
     println("Кол-во репостов: ${reposts.count}")
-    reposts.calculation(user)
+    reposts.calculation(1)
     println("Кол-во репостов: ${reposts.count}")
 
-    val comment = Comment(1, 2, "Комментарий", timestamp)
-    println(comment)
+    WallService.printAllComments()
+    println(WallService.createComment(1, Comment(0, 2, "Комментарий")))
+    println(WallService.createComment(1, Comment(0, 2, "Добавлен второй комментарий")))
+    println(WallService.createComment(1, Comment(0, 2, "Добавлен третий комментарий")))
 
-    WallService.createComment(1, comment)
+    WallService.reportComment(1, 2)
+    WallService.reportComment( 2, 2)
+    WallService.reportComment( 3, 6)
+    WallService.printAllComplaints()
+
+    println(WallService.findById(1))
 }
--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -5,6 +5,8 @@ import org.junit.Before
 import org.junit.Test
 import ru.netology.WallService.add
 import ru.netology.WallService.createComment
+import ru.netology.WallService.findById
+import ru.netology.WallService.reportComment
 import ru.netology.WallService.update
 
 class WallServiceTest {
@@ -14,50 +16,95 @@ class WallServiceTest {
         WallService.clear()
     }
 
+//    var posts = add(Post(1, 1, 1, "Bla-Bla"))
+//
+//    val result = posts.post_id
+//
+//    assertEquals(1, result)
     @Test
     fun addNewPost() {
-        var posts = add(Post(1, 1, 1, "Bla-Bla", true, true, false, false, null, timestamp))
+        var post1 = Post(1, 1, 1, "Bla-Bla")
 
-        val result = posts.id
+        val result = add(post1)
 
-        assertEquals(1, result)
+        assertEquals(Post(1, 1, 1, "Bla-Bla"), result)
     }
 
     @Test
     fun updatePostFalse() {
-        var post = add(Post(1, 1, 1, "Bla-Bla", true, true, false, false, null, timestamp))
-        var newPost = Post(2, 1, 3, "Update Bla-Bla", true, true, false, false, null, timestamp)
+        val post1 = add(Post(1, 1, 1, "Bla-Bla"))
 
-        val result = update(newPost)
+        val result = update(Post(2, 1, 3, "Update Bla-Bla"))
 
         assertEquals(false, result)
     }
 
     @Test
     fun updatePostTrue() {
-        var post = add(Post(1, 1, 1, "Bla-Bla", true, true, false, false, null, timestamp))
-        var newPost = Post(1, 1, 3, "Update Bla-Bla", true, true, false, false, null, timestamp)
+        val post1 = add(Post(1, 1, 1, "Bla-Bla"))
 
-        val result = update(newPost)
+        val result = update(Post(1, 1, 3, "Update Bla-Bla"))
 
         assertEquals(true, result)
     }
 
     @Test
-    fun createCommentTestCorrect() {
-        var post = add(Post(1, 1, 1, "Bla-Bla", true, true, false, false, null, timestamp))
-        var comment = Comment(1, 2, "Комментарий", timestamp)
+    fun createCommentTest() {
+        val post1 = add(Post(1, 1, 1, "Bla-Bla"))
+        val comment1 = Comment(1, 2, "Комментарий")
 
-        val result = createComment(1, comment)
+        val result = createComment(1, comment1)
 
-        assertEquals(Comment(1, 2, "Комментарий", timestamp), result)
+        assertEquals(Comment(1, 2, "Комментарий"), result)
     }
 
     @Test(expected = PostNotFoundException::class)
-    fun shouldThrow() {
-        var post = add(Post(1, 1, 1, "Bla-Bla", true, true, false, false, null, timestamp))
-        var comment = Comment(1, 2, "Комментарий", timestamp)
+    fun createCommentException() {
+        val post1 = add(Post(1, 1, 1, "Bla-Bla"))
+        val comment1 = Comment(1, 2, "Комментарий")
 
-        val result = createComment(2, comment)
+        val result = createComment(2, comment1)
+    }
+
+    @Test
+    fun reportCommentTestComment_id() {
+        val post1 = add(Post(1, 1, 1, "Bla-Bla"))
+        val comment1 = createComment(1, Comment(0, 2, "Комментарий"))
+
+        val result = reportComment(1, 3)
+
+        assertEquals(true, result)
+    }
+
+    @Test(expected = ReasonNotFoundException::class)
+    fun reportCommentTestReasonException() {
+        val post1 = add(Post(1, 1, 1, "Bla-Bla"))
+        val comment1 = createComment(1, Comment(0, 2, "Комментарий"))
+
+        val result = reportComment(1, 9)
+    }
+
+    @Test(expected = CommentNotFoundException::class)
+    fun reportCommentException() {
+        val post1 = add(Post(1, 1, 1, "Bla-Bla"))
+        val comment1 = createComment(1, Comment(0, 2, "Комментарий"))
+
+        val result = reportComment(2, 3)
+    }
+
+    @Test
+    fun findByIdTest() {
+        val post1 = add(Post(1, 1, 1, "Bla-Bla"))
+
+        val result = findById(1)
+
+        assertEquals(Post(2, 1, 1, "Bla-Bla"), result)
+    }
+
+    @Test(expected = PostNotFoundException::class)
+    fun findByIdException() {
+        val post1 = add(Post(1, 1, 1, "Bla-Bla"))
+
+        val result = findById(5)
     }
 }
